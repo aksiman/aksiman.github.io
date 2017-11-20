47c149f7d3149cd3161d8278400099436cb9002f
aksiman <q.aksiman@yandex.ru>
2017-11-20 18:16:58 +0300

    1

--- js/common.js
+++ js/common.js
@@ -58,7 +58,7 @@ $(function() {
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("pole").submit(function() { //Change
+	$(".pole").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
